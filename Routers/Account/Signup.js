bb1d7100a5c3caf7dc345b7df1ec03bf9e4270e3
MoosaAK <moosaak656834@gmail.com>
2019-09-12 00:57:54 +0500

    12/8/2019 6:00

--- Routers/Account/Signup.js
+++ Routers/Account/Signup.js
@@ -1,4 +1,459 @@
 
+// // import * as React from 'react';
+// // import * as firebase from "firebase"
+// // import fire from '..//Action/Action'
+// // import { Button, StyleSheet, Image, Text, TextInput, View } from 'react-native';
+// // // import {Permissions} from 'expo'
+// // import * as ImagePicker from "expo-image-picker"
+// // import * as Permissions from 'expo-permissions';
+// // import Constants from "expo-constants"
+// // import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+
+
+
+
+// // export default class Signup extends React.Component {
+
+// //     state = {
+// //         image: null,
+// //         email: '',
+// //         password: '',
+// //         errorMessage: null
+// //     };
+
+// //     handleSignUp = () => {
+// //         const { email, password } = this.state
+// //         firebase
+// //             .auth()
+// //             .createUserWithEmailAndPassword(email, password)
+// //             .then(user => this.props.navigation.navigate('Page'))
+// //             .catch(error => this.setState({ errorMessage: error.message }))
+// //     }
+
+
+// //     render() {
+// //         let { image } = this.state;
+
+// //         return (
+// //             <View style={styles.MainContainer} >
+// //                 <Text style={{ marginTop: 5, fontSize: 18, fontWeight: "bold" }}>CREATE A NEW ACCOUNT</Text>
+// //                 <View style={{ flex: 1, justifyContent: "center", alignItems: 'center' }}>
+
+// //                      {this.state.errorMessage &&
+// //                         <Text style={{ color: '#800000',alignContent:'center',fontSize:10,textAlign:'center' }}>
+// //                             {this.state.errorMessage}
+// //                         </Text>}
+// //                     {image &&
+// //                         <Image source={{ uri: image }}
+// //                             style={{
+// //                                 borderWidth: 1,
+// //                                 borderColor: 'rgba(0,0,0,0.2)',
+// //                                 alignItems: 'center',
+// //                                 justifyContent: 'center',
+// //                                 width: 80,
+// //                                 height: 80,
+// //                                 backgroundColor: '#fff',
+// //                                 borderRadius: 40,
+// //                             }}
+// //                         />}
+
+// //                     <TextInput
+// //                         placeholderTextColor="black"
+// //                         placeholder="Email Address"
+// //                         autoCapitalize="none"
+// //                         style={styles.textInputStyle}
+// //                         onChangeText={email => this.setState({ email })}
+// //                         value={this.state.email}
+// //                     />
+
+// //                     <TextInput
+// //                         secureTextEntry
+// //                         placeholderTextColor="black"
+// //                         placeholder="Password"
+// //                         autoCapitalize="none"
+// //                         value={this.state.password}
+// //                         style={styles.textInputStyle}
+// //                         onChangeText={password => this.setState({ password })}
+
+// //                     />
+
+// //                     <Text style={{ color: 'black', fontSize: 10, marginTop: 20 }}>Select Profile Picture</Text>
+// //                     <View style={{ flexDirection: 'row' }}>
+// //                         <TouchableOpacity
+// //                             style={{ marginTop: 10 }}
+// //                             onPress={this._pickImage}
+// //                         >
+// //                             <Image
+// //                                 style={{ width: 50, height: 50 }}
+// //                                 source={require('./assets/CameraRoll.png')}
+// //                             />
+// //                         </TouchableOpacity>
+
+
+// //                         <TouchableOpacity
+// //                             style={{ marginTop: 10, marginLeft: 20 }}
+// //                             onPress={() =>
+// //                                 this.props.navigation.navigate('Page2')
+// //                             }
+// //                         >
+// //                             <Image
+// //                                 style={{ width: 50, height: 50 }}
+// //                                 source={require('./assets/Camera.png')}
+// //                             />
+// //                         </TouchableOpacity>
+// //                     </View>
+// //                     <View flexDirection='row'>
+// //                         <TouchableOpacity
+// //                            onPress={() =>
+// //                                 this.props.navigation.navigate('Login')
+// //                             } activeOpacity={0.7} style={styles.button} >
+
+// //                             <Text style={styles.buttonText}>SIGN IN INSTEAD</Text>
+
+// //                         </TouchableOpacity>
+
+// //                         <TouchableOpacity
+// //                             onPress={this.handleSignUp}
+// //                             activeOpacity={0.7} style={styles.button} >
+
+// //                             <Text style={styles.buttonText}>SIGNUP</Text>
+
+// //                         </TouchableOpacity>
+
+// //                     </View>
+// //                 </View>
+
+// //             </View>
+// //         );
+// //     }
+
+
+// //     componentDidMount() {
+// //         this.getPermissionAsync();
+// //     }
+
+// //     getPermissionAsync = async () => {
+// //         if (Constants.platform.ios) {
+// //             const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+// //             if (status !== 'granted') {
+// //                 alert('Sorry, we need camera roll permissions to make this work!');
+// //             }
+// //         }
+// //     }
+
+// //     _pickImage = async () => {
+// //         let result = await ImagePicker.launchImageLibraryAsync({
+// //             mediaTypes: ImagePicker.MediaTypeOptions.All,
+// //             allowsEditing: true,
+// //             aspect: [4, 3],
+// //         });
+
+// //         console.log(result);
+
+// //         if (!result.cancelled) {
+// //             this.setState({ image: result.uri });
+// //         }
+// //     };
+// // }
+
+
+
+// // const styles = StyleSheet.create({
+
+// //     MainContainer: {
+
+// //         flex: 1,
+// //         justifyContent: 'center',
+// //         alignItems: 'center',
+// //         backgroundColor: '#69c2bb',
+// //         padding: 11
+
+// //     },
+
+// //     button: {
+// //         alignItems: 'center',
+// //         backgroundColor: 'black',
+// //         padding: 10,
+// //         width: 140,
+// //         marginTop: 12,
+// //         marginHorizontal: 10
+
+// //     },
+
+// //     text: {
+
+// //         color: '#fff'
+// //     },
+// //     textInputStyle: {
+
+// //         height: 37,
+// //         width: 250,
+// //         textAlign: 'center',
+// //         borderWidth: 1,
+// //         borderColor: 'black',
+// //         borderRadius: 8,
+// //         marginTop: 15
+// //     },
+// //     buttonText: {
+// //         color: '#69c2bb',
+// //         textAlign: 'center',
+// //         fontSize: 11
+// //     },
+// //     textStyle: {
+
+// //         color: '#000',
+// //         textAlign: 'center',
+// //         fontSize: 20
+
+// //     }
+// // });
+
+
+
+
+
+
+
+
+
+// import * as React from 'react';
+// import * as firebase from "firebase"
+// import fire from '..//Action/Action'
+// import { Button, StyleSheet, Image, Text, TextInput, View } from 'react-native';
+// // import {Permissions} from 'expo'
+// import * as ImagePicker from "expo-image-picker"
+// import * as Permissions from 'expo-permissions';
+// import Constants from "expo-constants"
+// import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+
+
+
+
+// export default class Signup extends React.Component {
+
+//     state = {
+//         image: null,
+//         name:'',
+//         email: '',
+//         password: '',
+//         errorMessage: null
+//     };
+
+//     handleSignUp = () => {
+//         const {email, password } = this.state
+//         firebase
+//             .auth()
+//             .createUserWithEmailAndPassword(email, password)
+//             .then(user => this.props.navigation.navigate('Page'))
+//             .catch(error => this.setState({ errorMessage: error.message }))
+//     }
+
+    
+
+//     render() {
+//         let { image } = this.state;
+
+//         return (
+//             <View style={styles.MainContainer} >
+//                 <Text style={{ marginTop: 5, fontSize: 18, fontWeight: "bold",marginTop:80 }}>CREATE A NEW ACCOUNT</Text>
+//                 <View style={{ flex: 1, justifyContent: "center", alignItems: 'center' }}>
+
+//                     {this.state.errorMessage &&
+//                         <Text style={{ color: '#800000', alignContent: 'center', fontSize: 14, textAlign: 'center' }}>
+//                             {this.state.errorMessage}
+//                         </Text>}
+//                     {image &&
+//                         <Image source={{ uri: image }}
+//                             style={{
+//                                 borderWidth: 1,
+//                                 borderColor: 'rgba(0,0,0,0.2)',
+//                                 alignItems: 'center',
+//                                 justifyContent: 'center',
+//                                 width: 80,
+//                                 height: 80,
+//                                 backgroundColor: '#fff',
+//                                 borderRadius: 40,
+//                             }}
+//                         />}
+//                     <TextInput
+//                         // secureTextEntry
+//                         placeholderTextColor="black"
+//                         placeholder="Full Name"
+//                         autoCapitalize="none"
+//                         // value={this.state.password}
+//                         style={styles.textInputStyle}
+
+//                     />
+//                     <TextInput
+//                         placeholderTextColor="black"
+//                         placeholder="Email Address"
+//                         autoCapitalize="none"
+//                         style={styles.textInputStyle}
+//                         onChangeText={email => this.setState({ email })}
+//                         value={this.state.email}
+//                     />
+
+//                     <TextInput
+//                         secureTextEntry
+//                         placeholderTextColor="black"
+//                         placeholder="Password"
+//                         autoCapitalize="none"
+//                         value={this.state.password}
+//                         style={styles.textInputStyle}
+//                         onChangeText={password => this.setState({ password })}
+
+//                     />
+
+
+//                     <Text style={{ color: 'black', fontSize: 10, marginTop: 20 }}>Select Profile Picture</Text>
+//                     <View style={{ flexDirection: 'row' }}>
+//                         <TouchableOpacity
+//                             style={{ marginTop: 10 }}
+//                             onPress={this._pickImage}
+//                         >
+//                             <Image
+//                                 style={{ width: 50, height: 50 }}
+//                                 source={require('./assets/CameraRoll.png')}
+//                             />
+//                         </TouchableOpacity>
+
+
+//                         <TouchableOpacity
+//                             style={{ marginTop: 10, marginLeft: 20 }}
+//                             onPress={() =>
+//                                 this.props.navigation.navigate('Page2')
+//                             }
+//                         >
+//                             <Image
+//                                 style={{ width: 50, height: 50 }}
+//                                 source={require('./assets/Camera.png')}
+//                             />
+//                         </TouchableOpacity>
+//                     </View>
+//                     <View flexDirection='row'>
+//                         <TouchableOpacity
+//                             onPress={() =>
+//                                 this.props.navigation.navigate('Login')
+//                             } activeOpacity={0.7} style={styles.button} >
+
+//                             <Text style={styles.buttonText}>SIGN IN INSTEAD</Text>
+
+//                         </TouchableOpacity>
+
+//                         <TouchableOpacity
+//                             onPress={this.handleSignUp}
+//                             activeOpacity={0.7} style={styles.button} >
+
+//                             <Text style={styles.buttonText}>SIGNUP</Text>
+
+//                         </TouchableOpacity>
+
+//                     </View>
+//                 </View>
+
+//             </View>
+//         );
+//     }
+
+
+//     componentDidMount() {
+//         this.getPermissionAsync();
+//     }
+
+//     getPermissionAsync = async () => {
+//         if (Constants.platform.ios) {
+//             const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+//             if (status !== 'granted') {
+//                 alert('Sorry, we need camera roll permissions to make this work!');
+//             }
+//         }
+//     }
+
+//     _pickImage = async () => {
+//         let result = await ImagePicker.launchImageLibraryAsync({
+//             mediaTypes: ImagePicker.MediaTypeOptions.All,
+//             allowsEditing: true,
+//             aspect: [4, 3],
+//         });
+
+//         console.log(result);
+
+//         if (!result.cancelled) {
+//             this.setState({ image: result.uri });
+//         }
+//     };
+// }
+
+
+
+// const styles = StyleSheet.create({
+
+//     MainContainer: {
+
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#69c2bb',
+//         padding: 11
+
+//     },
+
+//     button: {
+//         alignItems: 'center',
+//         backgroundColor: 'black',
+//         padding: 10,
+//         width: 140,
+//         marginTop: 12,
+//         marginHorizontal: 10
+
+//     },
+
+//     text: {
+
+//         color: '#fff'
+//     },
+//     textInputStyle: {
+
+//         height: 37,
+//         width: 250,
+//         textAlign: 'center',
+//         borderWidth: 1,
+//         borderColor: 'black',
+//         borderRadius: 8,
+//         marginTop: 15
+//     },
+//     buttonText: {
+//         color: '#69c2bb',
+//         textAlign: 'center',
+//         fontSize: 11
+//     },
+//     textStyle: {
+
+//         color: '#000',
+//         textAlign: 'center',
+//         fontSize: 20
+
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import * as React from 'react';
 import * as firebase from "firebase"
 import fire from '..//Action/Action'
@@ -18,13 +473,14 @@ export default class Signup extends React.Component {
 
     state = {
         image: null,
+        name:'',
         email: '',
         password: '',
         errorMessage: null
     };
 
     handleSignUp = () => {
-        const { email, password } = this.state
+        const {email, password } = this.state
         firebase
             .auth()
             .createUserWithEmailAndPassword(email, password)
@@ -32,17 +488,18 @@ export default class Signup extends React.Component {
             .catch(error => this.setState({ errorMessage: error.message }))
     }
 
+    
 
     render() {
         let { image } = this.state;
 
         return (
             <View style={styles.MainContainer} >
-                <Text style={{ marginTop: 5, fontSize: 18, fontWeight: "bold" }}>CREATE A NEW ACCOUNT</Text>
+                <Text style={{ marginTop: 5, fontSize: 18, fontWeight: "bold",marginTop:80 }}>CREATE A NEW ACCOUNT</Text>
                 <View style={{ flex: 1, justifyContent: "center", alignItems: 'center' }}>
 
-                     {this.state.errorMessage &&
-                        <Text style={{ color: '#800000',alignContent:'center',fontSize:10,textAlign:'center' }}>
+                    {this.state.errorMessage &&
+                        <Text style={{ color: '#800000', alignContent: 'center', fontSize: 14, textAlign: 'center' }}>
                             {this.state.errorMessage}
                         </Text>}
                     {image &&
@@ -58,7 +515,15 @@ export default class Signup extends React.Component {
                                 borderRadius: 40,
                             }}
                         />}
+                    <TextInput
+                        // secureTextEntry
+                        placeholderTextColor="black"
+                        placeholder="Full Name"
+                        autoCapitalize="none"
+                        // value={this.state.password}
+                        style={styles.textInputStyle}
 
+                    />
                     <TextInput
                         placeholderTextColor="black"
                         placeholder="Email Address"
@@ -78,6 +543,7 @@ export default class Signup extends React.Component {
                         onChangeText={password => this.setState({ password })}
 
                     />
+
 
                     <Text style={{ color: 'black', fontSize: 10, marginTop: 20 }}>Select Profile Picture</Text>
                     <View style={{ flexDirection: 'row' }}>
@@ -106,7 +572,7 @@ export default class Signup extends React.Component {
                     </View>
                     <View flexDirection='row'>
                         <TouchableOpacity
-                           onPress={() =>
+                            onPress={() =>
                                 this.props.navigation.navigate('Login')
                             } activeOpacity={0.7} style={styles.button} >
 
--- Routers/Page.js
+++ Routers/Page.js
@@ -1,3 +1,144 @@
+// // HOME
+// // Discription of this Page.js
+// // {Home + FourDifferentRouters > Image }
+
+
+// import React from 'react';
+// import { StyleSheet, Text, View, Image, Linking, Button } from 'react-native';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
+
+// export default class Page extends React.Component {
+//     render() {
+//         return (
+//             <View style={styles.container}>
+
+
+//                 <Text style={{ fontSize: 20, color: '#121149', fontWeight: "bold", marginTop: -100 }}>BLOOD BANK DASHBOARD</Text>
+//                 <View style={{ flexDirection: "row" }}>
+
+//                     <TouchableOpacity
+//                         onPress={() =>
+//                             this.props.navigation.navigate('DonorForm')
+//                         }
+//                     >
+//                         <Image
+//                             style={styles.image}
+
+//                             source={require('./assets/DonateBlood.png')}
+//                         />
+//                         <Text style={styles.textCaptiopn}>Donate Blood</Text>
+
+//                     </TouchableOpacity>
+
+//                     <TouchableOpacity
+//                         onPress={() =>
+//                             this.props.navigation.navigate('BloodBank')
+//                         }
+//                     >
+//                         <Image
+//                             style={styles.image}
+
+//                             source={require('./assets/BloodBank.png')}
+//                         />
+//                         <Text style={styles.textCaptiopn}>Blood Bank</Text>
+
+//                     </TouchableOpacity>
+
+//                 </View>
+//                 <View style={{ flexDirection: "row" }}>
+
+//                     <TouchableOpacity
+//                         onPress={() =>
+//                             this.props.navigation.navigate("ViewProfile")
+//                         }
+//                     >
+//                         <Image
+//                             style={styles.image}
+
+//                             source={require('./assets/userlogin.png')}
+//                         />
+//                         <Text style={styles.textCaptiopn}>View Profile</Text>
+//                     </TouchableOpacity>
+
+//                     <TouchableOpacity
+//                         onPress={() =>
+//                             // this.props.navigation.navigate('HelpDesk')
+//                             Linking.openURL('https://bot.dialogflow.com/44f230c3-a34b-4a7c-91f7-fe69c4cc3280 ')
+//                         }
+//                     >
+//                         <Image
+//                             style={styles.image}
+//                             source={require('./assets/HelpDesk3.png')}
+//                         />
+//                         <Text style={styles.textCaptiopn}>Help Desk</Text>
+//                     </TouchableOpacity>
+
+//                 </View>
+
+//             </View>
+//         );
+//     }
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: 'white',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+//     button: {
+//         alignItems: 'center',
+//         backgroundColor: 'white',
+//         padding: 10,
+//         paddingHorizontal: 120,
+//     },
+//     image: {
+//         borderWidth: 1,
+//         borderColor: 'white',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         width: 100,
+//         height: 100,
+//         backgroundColor: 'white',
+//         borderRadius: 50,
+//         marginTop: 40,
+//         marginHorizontal: 20,
+
+//     },
+//     textCaptiopn: {
+//         marginHorizontal: 10,
+//         color: '#121149',
+//         textAlign: 'center'
+//     }
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // HOME
 // Discription of this Page.js
 // {Home + FourDifferentRouters > Image }
